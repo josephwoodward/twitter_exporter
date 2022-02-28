e8b3678f2c197ce657a7a4d5a1c20eaceaff017b
Joseph Woodward <joseph.woodward@xeuse.com>
2022-02-28 03:33:42 +0000

    Add more program arguments

--- cmd/main.go
+++ cmd/main.go
@@ -18,20 +18,32 @@ var (
 	accessToken    = os.Getenv("TWITTER_ACCESS_TOKEN")
 	accessSecret   = os.Getenv("TWITTER_ACCESS_SECRET")
 
-	addr = flag.String("listen-address", ":8081", "The address to listen on for HTTP requests.")
-	user = flag.String("user", "_josephwoodward", "Twitter account name")
+	addr = flag.String("port", "8081", "The port to listen on for HTTP requests.")
+	user = flag.String("user", "", "Twitter account name")
+	path = flag.String("path", "/metrics", "Metrics path")
 )
 
 var profile *exporter.TwitterProfile
 
 func main() {
-
 	flag.Parse()
 
 	fmt.Printf("Looking up Twitter user '%s'\n", *user)
-	fmt.Printf("Metrics available on http://localhost%s/metrics\n", *addr)
 
 	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	if len(consumerKey) == 0 {
+		panic("TWITTER_CONSUMER_KEY not set")
+	}
+	if len(consumerSecret) == 0 {
+		panic("TWITTER_CONSUMER_SECRET not set")
+	}
+	if len(accessToken) == 0 {
+		panic("TWITTER_ACCESS_TOKEN not set")
+	}
+	if len(accessSecret) == 0 {
+		panic("TWITTER_ACCESS_SECRET not set")
+	}
+
 	token := oauth1.NewToken(accessToken, accessSecret)
 
 	profile = exporter.NewTwitterProfile(*user, config.Client(oauth1.NoContext, token))
@@ -39,6 +51,9 @@ func main() {
 	collector := exporter.NewCollector(profile)
 	prometheus.MustRegister(collector)
 
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	http.Handle(*path, promhttp.Handler())
+	fmt.Printf("Metrics available on http://localhost:%s%s\n", *addr, *path)
+
+	port := fmt.Sprintf(":%v", *addr)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
